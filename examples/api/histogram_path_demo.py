e541cf92d59c73b9a7a1384fd806e8a9aac1b488
Tobias Lang <tlang303@Tobiass-MacBook-Air.local>
2014-09-27 13:19:38 -0400

    Converted histogram_path_demo to function

--- examples/api/histogram_path_demo.py
+++ examples/api/histogram_path_demo.py
@@ -15,33 +15,47 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.path as path
 
-fig, ax = plt.subplots()
+def histogram_path_demo(ax, hist, bin_edges, **kwargs):
+    """
+    Example function to plot a histogram
 
-# histogram our data with numpy
+    Parameters
+    ----------
+    ax : Axes
+        The axes to draw to
+
+    hist : array
+        The values of the histogram.
+
+    bin_edges : array
+        The bin_edges (length(hist)+1).
+
+    **kwargs : dict
+        Dictionary of kwargs to pass to matplotlib.patches.PathPath
+    """
+
+    # get the corners of the rectangles for the histogram
+    left   = np.array(bin_edges[:-1])
+    right  = np.array(bin_edges[1:])
+    bottom = np.zeros(len(left))
+    top = bottom + hist
+    # we need a (numrects x numsides x 2) numpy array for the path helper
+    # function to build a compound path
+    XY = np.array([[left, left, right, right], [bottom, top, top, bottom]]).T
+
+    # get the Path object
+    barpath = path.Path.make_compound_path_from_polys(XY)
+    # make a patch out of it
+    patch = patches.PathPatch(barpath, **kwargs)
+    ax.add_patch(patch)
+    # update the view limits
+    ax.set_xlim(left[0], right[-1])
+    ax.set_ylim(bottom.min(), top.max())
+
+
+fig, ax = plt.subplots()
 data = np.random.randn(1000)
 n, bins = np.histogram(data, 50)
-
-# get the corners of the rectangles for the histogram
-left = np.array(bins[:-1])
-right = np.array(bins[1:])
-bottom = np.zeros(len(left))
-top = bottom + n
-
-
-# we need a (numrects x numsides x 2) numpy array for the path helper
-# function to build a compound path
-XY = np.array([[left, left, right, right], [bottom, top, top, bottom]]).T
-
-# get the Path object
-barpath = path.Path.make_compound_path_from_polys(XY)
-
-# make a patch out of it
-patch = patches.PathPatch(
-    barpath, facecolor='blue', edgecolor='gray', alpha=0.8)
-ax.add_patch(patch)
-
-# update the view limits
-ax.set_xlim(left[0], right[-1])
-ax.set_ylim(bottom.min(), top.max())
-
+histogram_path_demo(ax,n,bins, facecolor="blue", edgecolor="black", alpha=0.4)
 plt.show()
+
