bffd70929cad9552147731bb2c2dca9c6b9d3b3b
Tobey303 <tlang303@gmail.com>
2014-09-27 16:45:16 -0400

    Converted quad_bezier example to function

--- examples/api/quad_bezier.py
+++ examples/api/quad_bezier.py
@@ -2,16 +2,52 @@ import matplotlib.path as mpath
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
-Path = mpath.Path
+def quad_bezier(ax, vertices, **kwargs):
+    """
+    Example function to plot quadratic bezier curves.
+
+    Parameters
+    ----------
+    ax : Axes
+        The axes to draw to
+
+    vertices : array
+        The vertices for the bezier curves. Each quadratic curve requires 3 vertices
+
+    **kwargs : dict
+        Dictionary of kwargs to pass to matplotlib.patches.PathPatch
+    """
+    assert len(vertices) % 3 == 0, 'Vertex count must be multiple of 3 for quadratic bezier'
+    # build code list.
+    codes = []
+    for idx, v in enumerate(vertices):
+        if (idx % 3) == 0:
+            codes.append(mpath.Path.MOVETO)
+        else:
+            codes.append(mpath.Path.CURVE3)
+    my_path = mpath.Path(vertices, codes)
+    #set default values
+    kwargs["fc"]="none"
+    kwargs["transform"]=ax.transData
+    my_path_patch = mpatches.PathPatch(my_path,**kwargs)
+    ax.add_patch(my_path_patch)
+
 
 fig, ax = plt.subplots()
-pp1 = mpatches.PathPatch(
-    Path([(0, 0), (1, 0), (1, 1), (0, 0)],
-         [Path.MOVETO, Path.CURVE3, Path.CURVE3, Path.CLOSEPOLY]),
-    fc="none", transform=ax.transData)
+vertices = [(0.1, 0), (1, 0), (1, 0.5), (1, 0.5), (1,1), (0.1, 0.1)]
 
-ax.add_patch(pp1)
-ax.plot([0.75], [0.25], "ro")
-ax.set_title('The red point should be on the path')
+# Draw the quadratic bezier curves
+quad_bezier(ax, vertices, edgecolor="blue", linestyle="solid")
 
+# Plot the control lines
+# Rearrange list of tuples [(x0,y0),(x1,y1),...] into tuple of lists ([x0,x1,..],[y0,y1,..])
+line_coords = zip(*vertices)
+ax.plot(line_coords[0][:3],line_coords[1][:3],"k:") # first curve
+ax.plot(line_coords[0][3:],line_coords[1][3:],"k:") # second curve
+
+#grow axis limits for better display
+ax.set_xlim(-0.1,1.1)
+ax.set_ylim(-0.1,1.1)
 plt.show()
+
+
